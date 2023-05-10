673d1629bd9949d906d1254d486ddf4488b58e8e
Artin0123 <101245159+Artin0123@users.noreply.github.com>
2023-05-10 12:17:34 +0000

    1

--- cpe.cpp
+++ cpe.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
   // count代表輸入的數量，
-  int count = 0;
+  int count = 0, len = 0;
   // country代表各個國家名稱
   vector<string> country;
   // countrycount代表各個國家數量
@@ -15,34 +15,39 @@ int main() {
   // k代表當下的國家
   for (int k = 0; k <= count; k++) {
     getline(cin, name);
+    if (name.length() == 0) {
+      continue;
+    }
     for (int i = 0; i < name.length(); i++) {
-      // 偵測到第一個空白，代表國家名稱結束
-      if (name[i] == ' ') {
-        if (k == 1) {
-          country.push_back(name.substr(0, i));
-          countrycount.push_back(1);
-          break;
-        }
+      if (name[i] != ' ') {
+        len++;
+      }
+      if (name[i] == ' ' && len != 0) {
         // 比較是否有重複，j代表之前的國家
         bool repeat = false;
-        for (int j = 0; j < k; j++) {
-          if (country[j].compare(name.substr(0, i)) == 0) {
+        for (int j = 0; j < country.size(); j++) {
+          if (country[j].compare(name.substr(len, i)) == 0) {
             countrycount[j]++;
             repeat = true;
             break;
           }
         }
         if (repeat == false) {
-          country.push_back(name.substr(0, i));
+          country.push_back(name.substr(len, i));
           countrycount.push_back(1);
+          break;
         }
-        break;
       }
     }
   }
-  // for (int i = 0; i < country.size(); i++) {
-  //   if(country[i][1])
-  // }
+  for (int i = 0; i < country.size(); i++) {
+    for (int j = i + 1; j < country.size(); j++) {
+      if (int(country[j][0]) < int(country[i][0])) {
+        swap(country[i], country[j]);
+        swap(countrycount[i], countrycount[j]);
+      }
+    }
+  }
   for (int i = 0; i < country.size(); i++) {
     cout << country[i] << " " << countrycount[i] << endl;
   }
